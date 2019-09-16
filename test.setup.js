361bb107075fade8dd516433e31ca1c640be05d8
Ivan Grishin <r@exah.me>
2019-09-16 21:15:57 +0300

    🔧 Setup `node-fetch` and `abort-controller` for tests

---
+++ test.setup.js
@@ -0,0 +1,31 @@
+const URL = require('url')
+const fetch = require('node-fetch')
+const AbortController = require('abort-controller')
+
+if (!global.URL) {
+  global.URL = URL
+}
+
+if (!global.URLSearchParams) {
+  global.URLSearchParams = URLSearchParams
+}
+
+if (!global.fetch) {
+  global.fetch = fetch
+}
+
+if (!global.Headers) {
+  global.Headers = fetch.Headers
+}
+
+if (!global.Request) {
+  global.Request = fetch.Request
+}
+
+if (!global.Response) {
+  global.Response = fetch.Response
+}
+
+if (!global.AbortController) {
+  global.AbortController = AbortController
+}
